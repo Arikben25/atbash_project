f24a09f31c7f9e36b04b39d2fce5f091e61e8920
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-14 16:22:42 +0300

    added class 3

---
+++ atbash_project/Class3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atbash_project
+{
+    internal class Class3
+    {
+        public static void menager(string text, string[] dangerous_words)
+        {
+            string text_encr = Class1.Encription(text);
+            Program.ReferenceEquals(text_encr, dangerous_words);
+
+
+
+
+        } 
+    }
+}
