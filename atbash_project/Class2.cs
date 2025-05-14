fb2140fd6a6b56d4afc8bad4fb98495b8e48dbdb
ariel ben pazi <rbenpazi@gmail.com>
2025-05-14 15:58:55 +0300

    adedd_print

--- atbash_project/Class2.cs
+++ atbash_project/Class2.cs
@@ -8,5 +8,21 @@ namespace atbash_project
 {
     internal class Class2
     {
+        public static print_data_of_messege(string messege, int counte)
+        {   string WARNING = ""
+            if (counte <= 5)
+            {
+                WARNING = "WARNING"
+            }
+            else if (counte > 6 && counte <= 10)
+            {
+                WARNING = "DANGER!"
+            }
+            else (WARNING > 10){
+                WARNING = "ULTRA ALERT!"
+            }
+            Console.WriteLine($"The message is: {messege} \n The number of suspicious words is: {counte} \n The severity level of the message {WARNING}");   
+
+        }
     }
 }
