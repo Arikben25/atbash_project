ba7820ad1095d35d281d040fdf366d2218f25738
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-14 15:19:23 +0300

    edit the class progro

---
+++ atbash_project/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atbash_project
+{
+    internal class Class2
+    {
+    }
+}
